3afedd89b9ba861886a49d1b481f96b0f6ece005
bootstet@163.com <bootstet@163.com>
2024-09-30 18:48:53 +0800

    feat: 导入导出

--- index.js
+++ index.js
@@ -4,62 +4,15 @@ const path = require('path')
 const fs = require('fs')
 const fsExtra = require('fs-extra');
 const archiver = require('archiver')
+const { clearDir } = require('./utils')
+
+console.log('clearDir', clearDir)
 
 const app = express()
 
 const USER_HOME = process.env.HOME || process.env.USERPROFILE
 // const directoryPath = path.join(USER_HOME, 'Desktop\\images')
 const directoryPath = path.join(__dirname, 'images')
-
-
-/**
- * 删除文件夹下所有问价及将文件夹下所有文件清空
- * @param {*} path 
- */
-function emptyDir(path) {
-  const files = fs.readdirSync(path);
-  files.forEach(file => {
-      const filePath = `${path}/${file}`;
-      const stats = fs.statSync(filePath);
-      if (stats.isDirectory()) {
-          emptyDir(filePath);
-      } else {
-          fs.unlinkSync(filePath);
-          console.log(`删除${file}文件成功`);
-      }
-  });
-}
-
-/**
-* 删除指定路径下的所有空文件夹
-* @param {*} path 
-*/
-function rmEmptyDir(path, level=0) {
-  const files = fs.readdirSync(path);
-  if (files.length > 0) {
-      let tempFile = 0;
-      files.forEach(file => {
-          tempFile++;
-          rmEmptyDir(`${path}/${file}`, 1);
-      });
-      if (tempFile === files.length && level !== 0) {
-          fs.rmdirSync(path);
-      }
-  }
-  else {
-      level !==0 && fs.rmdirSync(path);
-  }
-}
-
-/**
-* 清空指定路径下的所有文件及文件夹
-* @param {*} path 
-*/
-function clearDir(path) {
-  emptyDir(path);
-  rmEmptyDir(path);
-
-}
 
 
 // 中间件，用于解析 JSON 格式的请求体
